f8e0a215589bd4899ba5faeab0031e2040443616
Vladislav <vladislav.bronzov@gmail.com>
2021-11-01 20:45:26 +0300

    fixes after first review

--- movies_admin/api/v1/views.py
+++ movies_admin/api/v1/views.py
@@ -4,38 +4,33 @@ from django.http import JsonResponse
 from django.views.generic.detail import BaseDetailView
 from django.views.generic.list import BaseListView
 
-from movies.models import Filmwork
+from movies.models import Filmwork, PersonRole
 
 
 class MoviesApiMixin:
     model = Filmwork
     http_method_names = ["get"]
 
+    def _aggregate_person(self, role):
+        return ArrayAgg(
+                "persons__full_name",
+                distinct=True,
+                filter=Q(personfilmwork__role=role))
+
     def get_queryset(self):
-        return Filmwork.objects.prefetch_related("film_genres", "persons").annotate(
-            actors=ArrayAgg(
-                "persons__full_name",
-                distinct=True,
-                filter=Q(personfilmwork__role="actor"),
-            ),
+        return Filmwork.objects.prefetch_related(
+            "film_genres",
+            "persons").annotate(
+            actors=self._aggregate_person(role=PersonRole.ACTOR),
             genres=ArrayAgg("film_genres__name", distinct=True),
-            directors=ArrayAgg(
-                "persons__full_name",
-                distinct=True,
-                filter=Q(personfilmwork__role="director"),
-            ),
-            writers=ArrayAgg(
-                "persons__full_name",
-                distinct=True,
-                filter=Q(personfilmwork__role="writer"),
-            ),
-        )
+            directors=self._aggregate_person(role=PersonRole.DIRECTOR),
+            writers=self._aggregate_person(role=PersonRole.WRITER))
 
     def render_to_response(self, context, **response_kwargs):
         return JsonResponse(context)
 
 
-class Movies(MoviesApiMixin, BaseListView):
+class MoviesListApi(MoviesApiMixin, BaseListView):
     paginate_by = 50
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -43,7 +38,7 @@ class Movies(MoviesApiMixin, BaseListView):
         paginator, page, queryset, is_paginated = self.paginate_queryset(
             input_queryset, self.paginate_by
         )
-        values = queryset.values(
+        values = queryset.values_list(
             "id",
             "title",
             "description",
@@ -68,17 +63,4 @@ class Movies(MoviesApiMixin, BaseListView):
 
 class MoviesDetailApi(MoviesApiMixin, BaseDetailView):
     def get_context_data(self, **kwargs):
-        film = kwargs["object"]
-        context = {
-            "id": film.id,
-            "title": film.title,
-            "description": film.description,
-            "creation_date": film.creation_date,
-            "rating": film.rating,
-            "type": film.type,
-            "genres": film.genres,
-            "actors": film.actors,
-            "directors": film.directors,
-            "writers": film.writers,
-        }
-        return context
+        return kwargs["object"]
